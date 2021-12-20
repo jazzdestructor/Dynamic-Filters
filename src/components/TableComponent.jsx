7f1acdfc9af66498425efb90c4636a8aba4ca691
jazzdestructor <jazzdestructor@gmail.com>
2021-12-20 14:12:14 +0530

    Second Commit

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import DataTable from "react-data-table-component";
 import FilterComponent from "./FilterComponent";
 
